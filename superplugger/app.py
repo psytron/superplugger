4894b5a81f14cbaa85bfffe708847df7cd0ba742
psytron <m@psytron.com>
2019-10-15 18:52:33 -0700

    first commit

---
+++ superplugger/app.py
@@ -0,0 +1,15 @@
+
+
+
+
+def default( *args , **kwargs ):
+    print("\n")
+    print("       +MM    smNy:          yMm/  dmmmmmmm/            dMd           ")
+    print("       oMN     .+dh.      ./:`oMm- ::::::MM+   `////////mMm////////`  ")
+    print("   ///+NMh//MMh///+///`   oMN            MM+   :MM:`````dMd`````:MM:  ")
+    print("     /MM+   MMh Mm/       oMN     Web    MM+   :MMmmmmmmMMMmmmmmmMM:  ")
+    print("    +NMs   .MMMNs. `/-    oMN     Sec    MM+   .yy-`````dMd`````-yy-  ")
+    print("  .yMNo.   .MMh.   -MM.   oMN            MM+            dMd           ")
+    print(" sh:       ::yhhhhhh/-    +md         +dddo`            HHH           ")
+    print(" Node X Start")
+    print(" Enter Identifier. ")
